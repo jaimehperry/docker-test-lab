7e59c2a9bb293f5169c4148ff522b305eabc6575
Jaime Perry <jaime@codescience.com>
2017-01-04 15:48:06 -0500

    testing parallel execution of tests

---
+++ codecept.conf.js
@@ -0,0 +1,24 @@
+exports.config = {
+  "tests": "./e2e/tests/*.js",
+  "testPath": "./e2e/tests/",
+  "timeout": 10000,
+  "output": "./e2e/output",
+  "helpers": {
+    "WebDriverIO": {
+      "url": "http://localhost",
+      "browser": "chrome"
+    },
+    "JSForce": { "require": "./e2e/helpers/jsforce_helper.js" }
+  },
+  "include": {
+    "I": "./e2e/config/steps_file.js",
+    "DemoPage": "./e2e/pages/DemoPage.js"
+  },
+  "bootstrap": false,
+  "mocha": {
+    "reporterOptions": {
+        "reportDir": "./e2e/output"
+    }
+  },
+  "name": "Codescience"
+}
